a868364eeedb8cb7f08d8681a86df3509fceabe9
Ishchuk <innerjflame@gmail.com>
2020-11-13 23:10:33 +0200

    ci 1

--- index.php
+++ index.php
@@ -4,12 +4,9 @@ class A
 {
     public function __construct($a, $b)
     {
-
-        if ('aa' == $a)
-        {
+        if ('aa' == $a) {
             echo 11;
         }
         return $a . $b;
-
     }
 }
