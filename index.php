52c68b89aae411068d2fa9de957c7e52b7b0dea5
Ishchuk <innerjflame@gmail.com>
2020-11-14 00:44:07 +0200

    ci 2

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <?php
-
 declare(strict_types=1);
-
 namespace App;
-
-class A
-{
-    public function __construct($a, $b)
+class A{
+    public function __construct($a,$b)
     {
-        if ('aa' === $a) {
-            echo 11;
+        if ('aa' == $a) {
+            echo  11;
         }
-        $c = 'dd';
-
+        $c = "dd" . A::class;
         return $a . $b;
     }
 }
